500843a1089fe486256eb933ff9b2c75bece8ff4
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-20 10:51:27 -0600

    put code into modules

---
+++ animals/penguin.py
@@ -0,0 +1,12 @@
+# packages from movements that exposes two classes
+from movements import IWalking, ISwimming
+
+class Penguin(IWalking, ISwimming):
+
+    def __init__(self, name):
+        ISwimming.__init__(self)
+        IWalking.__init__(self)
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name} the Penguin'
